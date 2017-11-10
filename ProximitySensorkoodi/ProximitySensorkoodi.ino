f5319ef042942c971ff73a536db223833db0be7d
PartyArti <t6syii00@students.oamk.fi>
2017-11-10 09:00:39 +0200

    dcvcs
    säätämistä

--- ProximitySensorkoodi/ProximitySensorkoodi.ino
+++ ProximitySensorkoodi/ProximitySensorkoodi.ino
@@ -36,6 +36,9 @@ Distributed as-is; no warranty is given.
 
 #include <Wire.h>
 #include <SparkFun_APDS9960.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
 
 // Global Variables
 SparkFun_APDS9960 apds = SparkFun_APDS9960();
